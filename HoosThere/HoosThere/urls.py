022a6578b2f6ecf57c64b7db44009075f2986ae2
Daniel Ayoub <dsa8cx@virginia.edu>
2019-02-11 15:20:28 -0500

    More setup (trying to follow tutorial)

--- HoosThere/HoosThere/urls.py
+++ HoosThere/HoosThere/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+
 
 urlpatterns = [
     path('studentmatching/', include('studentmatching.urls')),
