61b9d3409cdb1fa818ca1888ba308c37ef60329d
Forrest Feaser <fdf3kz@virginia.edu>
2019-02-23 12:32:06 -0500

    slight login improvement

--- studentmatching/urls.py
+++ studentmatching/urls.py
@@ -7,8 +7,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    url(r'^login/$', auth_views.LoginView.as_view(template_name='studentmatching/login.html'), name='login'),
-    url(r'^logout/$', auth_views.LogoutView.as_view(template_name='studentmatching/logout.html'), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='studentmatching/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='studentmatching/logout.html'), name='logout'),
+    path('admin/', admin.site.urls),
+    #url(r'^login/$', auth_views.LoginView.as_view(template_name='studentmatching/login.html'), name='login'),
+    #url(r'^logout/$', auth_views.LogoutView.as_view(template_name='studentmatching/logout.html'), name='logout'),
+    #url(r'^admin/', admin.site.urls),
     path('', include('social_django.urls', namespace='social')),
-    url(r'^admin/', admin.site.urls),
 ]
