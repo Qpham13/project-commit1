645f24fff0844a9126a2ad1c03ac98f4b73d8cd4
Qpham13 <31832530+Qpham13@users.noreply.github.com>
2019-02-25 14:42:12 -0500

    added user model with other database models

--- studentmatching/models.py
+++ studentmatching/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Organization(models.Model):
+    org_text = models.CharField(max_length=50)
+
+class classes(models.Model):
+    class_text = models.CharField(max_length=50)
+
+class user(models.Model):
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=20)
+    name = models.CharField(max_length=100)
+    email=models.CharField(max_length=100)
+    year=models.CharField(max_length=50)
+    classes= models.ForeignKey(classes,on_delete=models.CASCADE)
+    organizations=models.ForeignKey(Organization,on_delete=models.CASCADE)
+
+
+
+
+
