491c98d99e9f0d726c5d4ebb24ed3558e070d63e
Qpham13 <31832530+Qpham13@users.noreply.github.com>
2019-02-25 14:17:09 -0500

    Add files via upload

---
+++ models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Organization(models.Model):
+    org_text = models.CharField(max_length=50)
+
+class classes(models.Model):
+    class_text = models.CharField(max_length=50)
+
+class user(models.Model):
+    name = models.CharField(max_length=100)
+    email=models.CharField(max_length=100)
+    year=models.CharField(max_length=50)
+    classes= models.ForeignKey(classes,on_delete=models.CASCADE)
+    organizations=models.ForeignKey(Organization,on_delete=models.CASCADE)
+
+
+
+
+
